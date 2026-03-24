cbe37a0920dcc28218e2df4f514323ecb901be73
Luukvhoutjee <luukvhout06@gmail.com>
2026-03-24 11:50:18 +0100

    Test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gilde DevOps - Urenregistratie</title>
+    <title>Gilde DevOps - Urenregistratie test</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
