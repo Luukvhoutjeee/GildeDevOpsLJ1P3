893601477d3c26b440a3606a66bed99bb14ea9fa
Luukvhoutjee <luukvhout06@gmail.com>
2026-04-15 13:00:03 +0200

    fix

--- urenregistratie/Overzicht/index.php
+++ urenregistratie/Overzicht/index.php
@@ -5,24 +5,22 @@
 <head>
     <meta charset="UTF-8">
     <title>Overzicht</title>
-    <link rel="stylesheet" href="style.css">
-</head>
+    <link rel="stylesheet" href="../style.css"> </head>
 
 <body>
     <nav>
-        <img src="/Foto's/logo_gilde-solutions.svg" class="logo" alt="Logo Gilde DevOps Solutions" width="200px">
+        <img src="../Foto's/logo_gilde-solutions.svg" class="logo" alt="Logo Gilde DevOps Solutions" width="200px">
         <ul>
-            <li><a href="/Hoofdpagina/index.php">Home</a></li>
-            <li><a href="/Invullen/index.php">Uren Invullen</a></li>
-            <li><a href="/Overzicht/index.php">Bekijk Overzicht</a></li>
+            <li><a href="../Hoofdpagina/index.php">Home</a></li>
+            <li><a href="../Invullen/index.php">Uren Invullen</a></li>
+            <li><a href="index.php">Bekijk Overzicht</a></li>
         </ul>
         <div class="nav-spacer"></div>
     </nav>
 
     <main class="overzicht-container">
-    <h1>Ingevulde waarden</h1>
+    
     <?php  
-
         if ($_SERVER["REQUEST_METHOD"] == "POST")
         {
             $datum = $_POST['datum'];
@@ -31,6 +29,9 @@
             $uren = $_POST['aantal_uren'];
             $omschrijving = $_POST['omschrijving'];
         ?> 
+        
+        <h1>Ingevulde waarden</h1>
+        
         <table>
             <thead>
                 <tr>
@@ -43,11 +44,11 @@
             </thead>
             <tbody>
                 <tr>
-                    <td class="roze-tekst"><?php echo ($datum); ?></td>
-                    <td class="roze-tekst"><?php echo ($ov); ?></td>
-                    <td class="roze-tekst"><?php echo ($project); ?></td>
-                    <td class="roze-tekst"><?php echo ($uren); ?></td>
-                    <td class="roze-tekst"><?php echo ($omschrijving); ?></td>
+                    <td class="roze-tekst"><?php echo htmlspecialchars($datum); ?></td>
+                    <td class="roze-tekst"><?php echo htmlspecialchars($ov); ?></td>
+                    <td class="roze-tekst"><?php echo htmlspecialchars($project); ?></td>
+                    <td class="roze-tekst"><?php echo htmlspecialchars($uren); ?></td>
+                    <td class="roze-tekst"><?php echo htmlspecialchars($omschrijving); ?></td>
                 </tr>
             </tbody>
         </table>
@@ -56,12 +57,11 @@
         } else { ?>
             <h1 class="blauwe-tekst">Er zijn geen ingevulde waarden.</h1>
             <div class="form-action">
-                <a href="/Invullen/index.php" class="knop-wissen">Ga naar invullen</a>
+                <a href="../Invullen/index.php" class="knop-wissen">Ga naar invullen</a>
             </div>
 
         <?php } ?>
     </main>
     
 </body>
-
 </html>
